ad0fcf524fe2122e1263e2914796f02e0fa18b0f
Rajeshwar Madhu <rajeshwarmadhu@Rajeshwars-MBP.lan>
2024-01-09 19:54:51 -0600

    Created a Fun Rest Controller and mapped it with the default routing parameter to get a string welcome message as a response

---
+++ myCoolApp/src/main/java/com/luv2code/springboot/demo/myCoolApp/rest/FunRestController.java
@@ -0,0 +1,12 @@
+package com.luv2code.springboot.demo.myCoolApp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    @GetMapping("/")
+    public String greet(){
+        return "Hello World";
+    }
+}
